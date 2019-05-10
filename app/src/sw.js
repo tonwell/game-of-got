2d821da1fe1f7307a5475206cd50572528122a9a
Wellington <wellington.silva@b2wdigital.com>
2019-05-10 16:07:16 -0300

    configuring pwa

--- app/src/sw.js
+++ app/src/sw.js
@@ -1,6 +1,28 @@
+// eslint-disable-next-line no-undef
 importScripts('https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js');
 
+// eslint-disable-next-line no-undef
 if (workbox) {
+  workbox.precaching.precacheAndRoute([]);
+  
+  workbox.routing.registerRoute(
+      /\.(?:js|css|html)$/,
+      new workbox.strategies.StaleWhileRevalidate({
+          cacheName: "static-resources",
+      })
+  );
+  workbox.routing.registerRoute(
+    /\.(?:jpg|png|jpeg)$/,
+    new workbox.strategies.CacheFirst({
+        cacheName: "images",
+    })
+  );
+  workbox.routing.registerRoute(
+    'https://www.anapioficeandfire.com/api/characters?page=2&pageSize=20',
+    new workbox.strategies.StaleWhileRevalidate({
+      cacheName: "API",
+    })
+  );
   console.log(`Yay! Workbox is loaded 🎉`);
 } else {
   console.log(`Boo! Workbox didn't load 😬`);
---
+++ app/workbox-config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  "globDirectory": "build/",
+  "globPatterns": [
+    "**/*.{json,ico,html,js,css,jpg}"
+  ],
+  "swDest": "build/sw.js",
+  "swSrc": "src/sw.js"
+};
